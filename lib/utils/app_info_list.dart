c8f1448f49dc2d74f2894262bbc195b6b18a45e2
calvinnickholaa <nickholascalvin1@gmail.com>
2023-05-23 13:20:11 +0700

    Home Page

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,47 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotel-1.jpg',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'hotel-2.jpg',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'hotel-3.jpg',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "SMG", 'name': "Semarang"},
+    'to': {'code': "JKT", 'name': "Jakarta"},
+    'flying_time': '2H 20M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23
+  },
+  {
+    'from': {'code': "SBY", 'name': "Surabaya"},
+    'to': {'code': "SGP", 'name': "Singapura"},
+    'flying_time': '4H 30M',
+    'date': "1 MAY",
+    'departure_time': "11:00 AM",
+    "number": 10
+  },
+  {
+    'from': {'code': "BDG", 'name': "Bandung"},
+    'to': {'code': "KLM", 'name': "Kalimantan"},
+    'flying_time': '8H 50M',
+    'date': "1 MAY",
+    'departure_time': "09:00 PM",
+    "number": 12
+  },
+];
